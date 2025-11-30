e24cd50fa0036f758dcd0ca1d67ebb15cc112c87
janrey <w33haa2@gmail.com>
2025-11-30 21:58:27 +0800

    feat: cloud formation fix

--- api-catalog/start-container.sh
+++ api-catalog/start-container.sh
@@ -4,7 +4,12 @@ if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
-php artisan key:generate
+if [ -z "$APP_KEY" ]; then
+    # If APP_KEY is empty or unset, we want to generate one.
+    # But if it's empty (but set), we must unset it so Laravel reads the generated one from .env
+    unset APP_KEY
+    php artisan key:generate
+fi
 php artisan migrate --force
 php artisan db:seed --class=ProductSeeder --force
 
