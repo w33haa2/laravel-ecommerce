3f56a0554cfcdb5e49e85561559619dab7b80c88
janrey <w33haa2@gmail.com>
2025-11-30 22:24:25 +0800

    feat: cloud formation fix

--- api-checkout/start-container.sh
+++ api-checkout/start-container.sh
@@ -5,10 +5,13 @@ if [ ! -f .env ]; then
 fi
 
 if [ -z "$APP_KEY" ]; then
-    # If APP_KEY is empty or unset, we want to generate one.
-    # But if it's empty (but set), we must unset it so Laravel reads the generated one from .env
-    unset APP_KEY
-    php artisan key:generate
+    # Check if .env already has a valid key
+    if ! grep -q "^APP_KEY=base64:" .env; then
+        # If APP_KEY is empty or unset, we want to generate one.
+        # But if it's empty (but set), we must unset it so Laravel reads the generated one from .env
+        unset APP_KEY
+        php artisan key:generate
+    fi
 fi
 php artisan migrate --force
 
